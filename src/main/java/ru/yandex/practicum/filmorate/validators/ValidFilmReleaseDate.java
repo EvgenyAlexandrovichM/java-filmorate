869d301f779b4828d68844c9fd2f5d59a2251a4b
Evgeniy <smilevich@yandex.ru>
2025-03-18 08:21:15 +0300

    10 спринт. Начало разработки приложения по оценки фильмов с использованием Spring boot. 1 коммит

--- src/main/java/ru/yandex/practicum/filmorate/validators/ValidFilmReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validators/ValidFilmReleaseDate.java
@@ -7,7 +7,7 @@ import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = {FilmReleaseDateValidator.class})
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Target({ ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidFilmReleaseDate {
     String message() default "Дата релиза должна быть не раньше 28 декабря 1895 года";
